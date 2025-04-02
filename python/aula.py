58fce2cb8d82d0006adc99ca6fb8de9fd8037145
Weslley Moreira <116123420+wesp1201@users.noreply.github.com>
2025-04-01 21:16:15 -0300

    Add files via upload

---
+++ python/aula.py
@@ -0,0 +1,8 @@
+contador = 1
+resposta = "s"
+while (contador <=5) :
+    x = int(input("Digite um valor para x: "))
+    r = x * 3
+    print ("O valor de R é: ", r)
+    contador = contador + 1
+    resposta = input("Deseja continuar? ")
---
+++ python/numeros.py
@@ -0,0 +1,9 @@
+a = int(input("Entre com o primeiro valor: "))
+b = int(input("Entre com o segundo valor: "))
+
+if (a > b):
+    print ("O primeiro valor é o maior")
+if (b > a):
+    print ("O segundo valor é o maior")
+if (a == b):
+    print ("Os numeros são iguais")    
