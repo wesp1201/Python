da57d7aecb1042317267b97dd434481a57b190fe
Weslley Moreira <116123420+wesp1201@users.noreply.github.com>
2025-04-09 13:53:03 -0300

    Delete python directory

--- python/numeros.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Entre com o primeiro valor: "))
-b = int(input("Entre com o segundo valor: "))
-
-if (a > b):
-    print ("O primeiro valor é o maior")
-if (b > a):
-    print ("O segundo valor é o maior")
-if (a == b):
-    print ("Os numeros são iguais")    
