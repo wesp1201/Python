465751283ecf7724b510de0e6357dd80e299dbb1
Weslley Moreira <116123420+wesp1201@users.noreply.github.com>
2025-04-16 12:26:30 -0300

    Delete python directory

--- python/whileEx1.py
+++
@@ -1,6 +0,0 @@
-num = 0
-
-while(num <= 10):
-    if (num % 2 == 0):
-        print(num)
-    num += 1
--- python/whileEx5.py
+++
@@ -1,8 +0,0 @@
-num = int(input('Digite um numero: '))
-
-fatorial = 1
-i = 1
-while(i <= num):
-    fatorial *= i
-    i += 1
-print('O fatorial de', num, 'é', fatorial)
